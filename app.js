2c655ee296fa7132e187f58e708539eaf13331f9
Hannah Kim <hannahsohkim@gmail.com>
2019-03-11 23:09:38 -0700

    Add new campground route

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 var express = require ('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+
+var campgrounds = [
+  {name: 'Lakesdale', image: 'https://pixabay.com/get/e03db50f2bfc1c22d2524518b7444795ea76e5d004b0144497f3c679a2eeb7_340.jpg'},
+  {name: 'Ventura Ranch', image: 'https://pixabay.com/get/ef3cb00b2af01c22d2524518b7444795ea76e5d004b0144497f3c479a3efb7_340.jpg'},
+  {name: 'Jellystone Park', image: 'https://farm1.staticflickr.com/82/225912054_690e32830d.jpg'},
+  {name: 'Lake Rudolph', image: 'https://farm2.staticflickr.com/1363/1342367857_2fd12531e7.jpg'},
+];
 
 app.get('/', (req, res) => {
   res.render('landing');
 })
 
 app.get('/campgrounds', (req, res) => {
-  var campgrounds = [
-    {name: 'Salmon Creek', image: 'https://www.photosforclass.com/download/pixabay-691424?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2Fef3cb00b2af01c22d2524518b7444795ea76e5d004b0144497f2c478a2efb0_960.jpg&user=Free-Photos'},
-    {name: 'Lakesdale', image: 'https://www.photosforclass.com/download/pixabay-1851092?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2Fe83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104490f2c37ca6e8b7bd_960.jpg&user=Pexels'},
-    {name: 'Ventura Ranch', image: 'https://www.photosforclass.com/download/flickr-2602356334'},
-    {name: 'Jellystone Park', image: 'https://www.photosforclass.com/download/flickr-14707566622'},
-    {name: 'Lake Rudolph', image: 'https://www.photosforclass.com/download/pixabay-1851092?webUrl=https%3A%2F%2Fpixabay.com%2Fget%2Fe83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104490f2c371a2eab4ba_960.jpg&user=Pexels'},
-  ];
-
   res.render('campgrounds', {campgrounds: campgrounds});
+})
+
+app.post('/campgrounds', (req, res) => {
+  var name = req.body.name;
+  var image = req.body.image;
+  var newCampground = {name: name, image: image};
+  campgrounds.push(newCampground);
+  res.redirect('/campgrounds')
+})
+
+app.get('/campgrounds/new', (req, res) => {
+  res.render('new');
 })
 
 var port = 3000;
